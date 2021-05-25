109135f11a4ed998eae6c86f79227228180dfd97
Mika <crazy8239@163.com>
2021-05-25 11:00:28 +0800

    Initial Commit

---
+++ ReadAndDictation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadAndDictation
+//
+//  Created by 江美佳 on 2021/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
